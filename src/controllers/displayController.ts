ae0552502664d340cd1f45c5b8d46a62e375a811
Chris <dawarr23-boop@users.noreply.github.com>
2026-03-05 00:56:45 +0100

    fix: authorizationStatus in getDisplayByIdentifier attributes laden

--- src/controllers/displayController.ts
+++ src/controllers/displayController.ts
@@ -132,6 +132,7 @@ export const getDisplayByIdentifier = async (
         'identifier',
         'description',
         'isActive',
+        'authorizationStatus',
         'tickerText',
         'tickerTransit',
         'tickerTraffic',
